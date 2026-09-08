10d1646dbccf3303702f7591615ebff06f1657c9
Kuboja Mabuba <danielkubojah04@gmail.com>
2026-09-08 12:21:51 +0530

    Time: 28 ms (47.87%) | Memory: 18.1 MB (39.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        queue<pair<int, int>> q;
+        int ans = 0;
+
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    q.push({i, j});
+                    grid[i][j] = 0;
+                    ans++;
+
+                    while(!q.empty()){
+                        int sz = q.size();
+                        while(sz--){
+                            auto [i, j] = q.front();
+                            q.pop();
+
+                            int dx[4] = {-1, 0, 1, 0};
+                            int dy[4] = {0, -1, 0, 1};
+
+                            for(int k = 0; k < 4; k++){
+                                int ni = i + dx[k];
+                                int nj = j + dy[k];
+                                if(ni >= 0 && ni < grid.size() && nj >= 0 && nj < grid[0].size() && grid[ni][nj] == '1'){
+                                    grid[ni][nj] = '0';
+                                    q.push({ni, nj});
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        
-        return numIslands;
-    }
-    
-private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
-            return;
-        }
-        
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j); 
-        dfs(grid, i, j + 1); 
-        dfs(grid, i, j - 1); 
+        return ans;
     }
 };
